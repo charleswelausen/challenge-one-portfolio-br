4b0caa5432126b3c3a747e77e23ccd9eebba0ad3
charleswelausen <cwelausen@hotmail.com>
2024-05-18 17:49:43 -0300

    concluido o challenge do portfolio

--- validate.js
+++ validate.js
@@ -1,1 +1,26 @@
-//Seu JavaScript de validação aqui
+//Seu JavaScript de validação aqui// 
+
+const formulario = document.querySelector(".formcontato__form");
+const emailInput = document.querySelector("#email");
+
+formulario.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if (!emailValido(emailInput.value)) {
+        alert('Este e-mail não é válido');
+        return;
+    }
+
+    formulario.submit()
+});
+
+function emailValido(email) {
+    const regex = new RegExp(
+        /^[a-zA-Z0-9]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    );
+
+    if (regex.test(email)) {
+        return true;
+    }
+    return false;
+}
